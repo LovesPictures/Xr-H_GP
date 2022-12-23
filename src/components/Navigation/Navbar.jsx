6ccdea2416bf66c8dca31abb7b6878443a44bf64
Jude <lovespictures2@gmail.com>
2022-12-23 13:26:37 +0000

    Updated Nav and Home css

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import HomeIcon from "@material-ui/icons/Home";
-// import HomeIcon from "../Icons/homeIcon";
 import PersonIcon from "@material-ui/icons/Person";
-// import FaceIcon from "@material-ui/icons/Face";
 import MapIcon from "@material-ui/icons/Map";
 import BubbleChartIcon from "@material-ui/icons/BubbleChart";
-// import CollectionsIcon from "@material-ui/icons/Collections";
+// import CollectionsIcon from "@material-ui/icons/Gallery";
+import CollectionsBookmark from "@material-ui/icons/CollectionsBookmark";
 import AlternateEmailIcon from "@material-ui/icons/AlternateEmail";
-// import MenuIcon from '@material-ui/icons/icons-material/Menu';
 
-// import icons form icons
+// import icons from icons
 // import Icons from "../img/Icons";
 
 import "./Navbar.css";
@@ -30,16 +28,19 @@ const Navbar = () => {
           <Link to="/about">
             <PersonIcon alt="About" />
           </Link>
+
           <Link to="/mapData">
             <MapIcon alt="Map data" />
           </Link>
+
           <Link to="/posters">
             <BubbleChartIcon alt="Digital Posters" />
           </Link>
-          {/* <Link to="/posters">
-            <CollectionsIcon alt="Digital Posters" />
-          </Link> */}
-          {/* <Link to="/gallery">Gallery</Link> */}
+
+          <Link to="/Gallery">
+            <CollectionsBookmark alt="Collections" />
+          </Link>
+
           <Link to="/contact">
             <AlternateEmailIcon />
           </Link>
